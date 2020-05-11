95ae36378b8fe8071a1d5fa3c4290669ad8615c5
Artem Shulyakov <a.shulyakov@foodplexgroup.ru>
2020-05-11 14:30:10 +0300

    module2-task1

--- source/js/modules/doc-ready.js
+++ source/js/modules/doc-ready.js
@@ -1,3 +1,11 @@
 export default () => document.addEventListener(`DOMContentLoaded`, () => {
   document.body.classList.add(`load`);
+
+  const listItems = document.querySelectorAll(`.rules__item p`);
+  const lastListItem = listItems[listItems.length - 1];
+  const rulesBtn = document.querySelector(`.rules__link`);
+
+  lastListItem.onanimationend = () => {
+    rulesBtn.classList.add(`rules__link--showed`);
+  };
 });
