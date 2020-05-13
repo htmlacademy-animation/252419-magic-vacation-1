37f89e75fd3aa9f9336ba1db1a6b14d75ea03294
Artem Shulyakov <a.shulyakov@foodplexgroup.ru>
2020-05-13 17:36:25 +0300

    module2-task2

--- source/js/modules/doc-ready.js
+++ source/js/modules/doc-ready.js
@@ -1,6 +1,9 @@
+import {TypographyBuild} from './typography-builder';
+
 export default () => document.addEventListener(`DOMContentLoaded`, () => {
   document.body.classList.add(`load`);
 
+  // module 2 task 1
   const listItems = document.querySelectorAll(`.rules__item p`);
   const lastListItem = listItems[listItems.length - 1];
   const rulesBtn = document.querySelector(`.rules__link`);
@@ -8,4 +11,27 @@ export default () => document.addEventListener(`DOMContentLoaded`, () => {
   lastListItem.onanimationend = () => {
     rulesBtn.classList.add(`rules__link--showed`);
   };
+
+  // module 2 task 2
+  const animationTopScreenTextLine = new TypographyBuild({
+    elementSelector: `.intro__title`,
+    timer: 350,
+    classForActivate: `showed`,
+    property: `transform`,
+  });
+
+  const animationTopScreenDateLine = new TypographyBuild({
+    elementSelector: `.intro__date`,
+    timer: 350,
+    classForActivate: `showed`,
+    property: `transform`,
+  });
+
+  setTimeout(() => {
+    animationTopScreenTextLine.runAnimation();
+  }, 500);
+
+  setTimeout(() => {
+    animationTopScreenDateLine.runAnimation();
+  }, 1500);
 });
---
+++ source/js/modules/typography-builder.js
@@ -0,0 +1,83 @@
+export class TypographyBuild {
+  constructor({
+    elementSelector = `.intro__title`,
+    timer = 500,
+    classForActivate = `showed`,
+    property = `transform`,
+  } = {}) {
+    this._elementSelector = elementSelector;
+    this._timer = timer;
+    this._classForActivate = classForActivate;
+    this._property = property;
+    this._element = document.querySelector(this._elementSelector);
+    this._iterationCount = 0;
+    this._staticDelay = 0;
+
+    this._prePareText();
+  }
+
+  _createElement(letter, delay) {
+    const span = document.createElement(`span`);
+
+    span.textContent = letter;
+    span.style.transition = `${this._property} ${this._timer}ms ease ${delay}ms`;
+
+    return span;
+  }
+
+  get _delayCreator() {
+    if (this._iterationCount >= 3) {
+      this._iterationCount = 0;
+      this._staticDelay += 150;
+    }
+
+    this._iterationCount += 1;
+    switch (this._iterationCount) {
+      case 1:
+        return 50 + this._staticDelay;
+      case 3:
+        return 100 + this._staticDelay;
+      case 2:
+        return 150 + this._staticDelay;
+      default:
+        return 0;
+    }
+  }
+
+  _prePareText() {
+    if (!this._element) {
+      return;
+    }
+
+    const text = this._element.textContent
+      .trim()
+      .split(` `)
+      .filter((latter) => latter !== ``);
+
+    const content = text.reduce((fragmentParent, word) => {
+      const wordElement = Array.from(word)
+        .reduce((fragment, latter) => {
+          fragment.appendChild(this._createElement(latter, this._delayCreator));
+          return fragment;
+        }, document.createDocumentFragment());
+
+      const wordContainer = document.createElement(`span`);
+
+      wordContainer.classList.add(`text__word`);
+      wordContainer.appendChild(wordElement);
+      fragmentParent.appendChild(wordContainer);
+
+      return fragmentParent;
+    }, document.createDocumentFragment());
+
+    this._element.innerHTML = ``;
+    this._element.appendChild(content);
+  }
+
+  runAnimation() {
+    if (!this._element) {
+      return;
+    }
+    this._element.classList.add(this._classForActivate);
+  }
+}
